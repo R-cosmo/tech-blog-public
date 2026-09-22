9a2e9ccaecae9a6fc01523c28540b45dbd3735e0
LeyuanWang <ccpfighter@gmail.com>
2026-09-22 22:10:22 +1000

    build setup

--- apps/web/src/utils/base-url.ts
+++ apps/web/src/utils/base-url.ts
@@ -2,5 +2,6 @@
 export function getBaseUrl(): string {
   if (process.env.NEXT_PUBLIC_SITE_URL) return process.env.NEXT_PUBLIC_SITE_URL;
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+  if (process.env.RENDER_EXTERNAL_URL) return process.env.RENDER_EXTERNAL_URL;
   return "http://localhost:3001";
 }
